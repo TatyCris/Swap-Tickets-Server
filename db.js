f29b3e993c119942f40438e96f4579a335f4877c
TATY <tatyccsilva@gmail.com>
2019-07-15 12:04:44 +0200

    First commit

---
+++ db.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize')
+
+const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/tikets-now'
+const sequelize = new Sequelize(databaseUrl, { define: { timestamps: false } })
+
+sequelize
+    .sync()
+    // .sync({ force: true })
+    .then(()=> console.log('Database schema updated'))
+    .catch(console.error)
+
+module.exports = sequelize
