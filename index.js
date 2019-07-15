6516f2977fc495cc054edd3384f930bb4cae38cd
TATY <tatyccsilva@gmail.com>
2019-07-15 19:58:50 +0200

    Create tickets model and router

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const db = require('./db')
 const userRouter = require('./user/router')
 const authRouter = require('./auth/router')
 const eventsRouter = require('./events/router')
+const ticketsRouter = require('./tickets/router')
 
 
 const app = express()
@@ -25,3 +26,4 @@ app.listen(port, onListen)
 app.use(userRouter)
 app.use(authRouter)
 app.use(eventsRouter)
+app.use(ticketsRouter)
